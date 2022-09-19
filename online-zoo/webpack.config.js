e54ac5bcc871d13f7be5f16e477fa17b7199205d
Alesia <alesia.litvinenko@gmail.com>
2022-09-19 23:46:47 +0300

    feat: add block background & slider

--- online-zoo/webpack.config.js
+++ online-zoo/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
       template: "./pages/main/index.html",
+      // chunks: ["index"],
     }),
     new HtmlWebpackHarddiskPlugin(),
   ],
@@ -37,6 +38,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(html)$/i,
+        use: ["html-loader"],
       },
     ],
   },
